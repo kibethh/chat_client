234076b8d681fa047ae74d2bf8c84120118203ea
kibethh <kibethh@gmail.com>
2022-12-26 22:37:53 +0000

    used useSubscription hook

--- src/graphql/hooks.js
+++ src/graphql/hooks.js
@@ -1,6 +1,10 @@
-import { useMutation, useQuery } from "@apollo/client";
+import { useMutation, useQuery, useSubscription } from "@apollo/client";
 import { getAccessToken } from "../auth";
-import { ADD_MESSAGE_MUTATION, MESSAGES_QUERY } from "./queries";
+import {
+  ADD_MESSAGE_MUTATION,
+  MESSAGES_QUERY,
+  MESSAGE_ADDED_SUBSCRIPTION,
+} from "./queries";
 
 export function useAddMessage() {
   const [mutate] = useMutation(ADD_MESSAGE_MUTATION);
@@ -13,13 +17,13 @@ export function useAddMessage() {
         context: {
           headers: { Authorization: "Bearer " + getAccessToken() },
         },
-        update: (cache, { data: { message } }) => {
-          cache.updateQuery({ query: MESSAGES_QUERY }, ({ messages }) => {
-            return {
-              messages: [...messages, message],
-            };
-          });
-        },
+        // update: (cache, { data: { message } }) => {
+        //   cache.updateQuery({ query: MESSAGES_QUERY }, ({ messages }) => {
+        //     return {
+        //       messages: [...messages, message],
+        //     };
+        //   });
+        // },
       });
       return message;
     },
@@ -30,6 +34,16 @@ export function useMessages() {
   const { data } = useQuery(MESSAGES_QUERY, {
     context: {
       headers: { Authorization: "Bearer " + getAccessToken() },
+    },
+  });
+  useSubscription(MESSAGE_ADDED_SUBSCRIPTION, {
+    onSubscriptionData: ({ client, subscriptionData }) => {
+      const message = subscriptionData.data.message;
+      client.cache.updateQuery({ query: MESSAGES_QUERY }, ({ messages }) => {
+        return {
+          messages: [...messages, message],
+        };
+      });
     },
   });
   return {
